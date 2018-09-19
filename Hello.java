fb880f6835289114e856bc1498a9d6da11573521
dylan <605501479@qq.com>
2018-09-20 04:12:13 +0800

    我再一次的提交了

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 ﻿public class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello World	");
-		System.out.println("www.java.com");
+		System.out.println("www.Hello.com");
 	}
 	
 }
